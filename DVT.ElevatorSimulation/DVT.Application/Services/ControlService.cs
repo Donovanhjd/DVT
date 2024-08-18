42770951fc2815e68b4858dbe4a6d970f86b35f6
Donovan <jddeklerk0@gmail.com>
2024-08-18 19:59:55 +0200

    Added call elevator logic and validation

--- DVT.ElevatorSimulation/DVT.Application/Services/ControlService.cs
+++ DVT.ElevatorSimulation/DVT.Application/Services/ControlService.cs
@@ -9,7 +9,8 @@ public class ControlService(IPassengerService passengerService, IElevatorService
 
     public async Task CallElevator()
     {
-        //TODO: Call all elevators
+        var floorNumber = GetValidatedIntInput("Enter floor number to call elevator:", 1, 10);
+        await _elevatorService.CallElevator(floorNumber)!;
     }
 
     public async Task AddPassengers()
@@ -24,12 +25,67 @@ public class ControlService(IPassengerService passengerService, IElevatorService
 
     private int GetValidatedIntInput(string prompt, int? minValue, int? maxValue)
     {
-        //TODO: Vaidate input
+        int value;
+
+        if (maxValue == 0)
+        {
+            return default;
+        }
+
+        do
+        {
+            Console.Write($"{prompt} ({minValue} - {maxValue}): ");
+            string? input = Console.ReadLine();
+            if (int.TryParse(input, out value) && value >= minValue && value <= maxValue)
+            {
+                break;
+            }
+            else
+            {
+                Console.WriteLine($"Invalid input. Please enter a number between {minValue} and {maxValue}.");
+            }
+        } while (true);
+
+        return value;
     }
 
     public async Task StartSimulation()
     {
-        //TODO: Start application with options
+        await _elevatorService.AddElevators();
+
+        while (true)
+        {
+            Console.Clear();
+            await _elevatorService.DisplayElevatorStatus();
+            Console.WriteLine("Options:");
+            Console.WriteLine("1. Call Elevator");
+            Console.WriteLine("2. Add Passengers");
+            Console.WriteLine("3. Remove Passengers");
+            Console.WriteLine("4. Exit");
+
+            var choice = GetValidatedIntInput($"Please choose an option between: ", 1, 4);
+
+            switch (choice)
+            {
+                case 1:
+                    await CallElevator();
+                    break;
+                case 2:
+                    await AddPassengers();
+                    break;
+                case 3:
+                    await RemovePassengers();
+                    break;
+                case 4:
+                    return;
+                default:
+                    Console.WriteLine("Invalid option. Please try again.");
+                    break;
+            }
+
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
     }
 }
 
