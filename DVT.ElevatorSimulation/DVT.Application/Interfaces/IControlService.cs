31714ce1a437cb889c77847ce00b59fafc1addc3
Donovan <jddeklerk0@gmail.com>
2024-08-18 19:54:00 +0200

    Added Control Service and updated the dependency injection

---
+++ DVT.ElevatorSimulation/DVT.Application/Interfaces/IControlService.cs
@@ -0,0 +1,10 @@
+﻿namespace DVT.Application.Interfaces
+{
+    public interface IControlService
+    {
+        Task AddPassengers();
+        Task CallElevator();
+        Task RemovePassengers();
+        Task StartSimulation();
+    }
+}
---
+++ DVT.ElevatorSimulation/DVT.Application/Services/ControlService.cs
@@ -0,0 +1,34 @@
+﻿using DVT.Application.Interfaces;
+
+namespace DVT.Application.Services;
+
+public class ControlService(IPassengerService passengerService, IElevatorService elevatorService) : IControlService
+{
+    private readonly IPassengerService _passengerService = passengerService;
+    private readonly IElevatorService _elevatorService = elevatorService;
+
+    public async Task CallElevator()
+    {
+        //TODO: Call all elevators
+    }
+
+    public async Task AddPassengers()
+    {
+        //TODO: Call add passenger service
+    }
+
+    public async Task RemovePassengers()
+    {
+        //TODO: call remove passenger from service
+    }
+
+    private int GetValidatedIntInput(string prompt, int? minValue, int? maxValue)
+    {
+        //TODO: Vaidate input
+    }
+
+    public async Task StartSimulation()
+    {
+        //TODO: Start application with options
+    }
+}
