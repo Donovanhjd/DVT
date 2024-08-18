b32c55e32c201726c3daaaa8c37c6a332517a977
Donovan <jddeklerk0@gmail.com>
2024-08-18 19:08:14 +0200

    Added a in memory data base with a repository for basic crud operations via IRepo

---
+++ DVT.ElevatorSimulation/DVT.Infrastructure/Interfaces/IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace DVT.Infrastructure.Interfaces;
+
+public interface IRepository<T> where T : class
+{
+    Task<T> GetById(int id);
+    Task<IEnumerable<T>> GetAll();
+    Task Add(T entity);
+    Task AddMultiple(IEnumerable<T> entities);
+    Task Update(T entity);
+    Task Delete(int id);
+}
